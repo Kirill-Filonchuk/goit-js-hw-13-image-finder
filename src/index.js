a1f9f97761a889cf0a5a129a126f84a4084ae243
Kirill Filonchuk <kir.realtor@gmail.com>
2021-07-12 17:52:09 +0300

    continue

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import './sass/main.scss';
+
+import getRefs from './js/refs';
+import ApiServicePixabey from './js/apiService';
+import photoCardMarkup from './templates/card.hbs';
+
+const apiServicePixabey = new ApiServicePixabey()
+
+const refs = getRefs();
+
+refs.btnLoad.addEventListener('click', onLoadMore);
+
+refs.searchForm.addEventListener('submit', sendSearch);
+
+function sendSearch(e) {
+    e.preventDefault();
+    // clearPageOnNewSearch();
+    apiServicePixabey.resetPage();
+ const inputData = e.currentTarget;
+    apiServicePixabey.request = inputData.elements.query.value;
+    if (apiServicePixabey.request === ' ' || apiServicePixabey.request === '' ) {
+        return alert('Вы ввели пробел! Введите свой запрос')
+    }
+    apiServicePixabey.getApiCards().then(photo => {
+        clearPageOnNewSearch();
+        renderPhotoGalery(photo)
+    })
+    // Т.К. асинхронная ф-ция на свое место ВСЕГДА возвращает промис, то к ней я применяю THEN
+   
+}
+
+
+
+function onLoadMore() {
+    apiServicePixabey.getApiCards().then(renderPhotoGalery);
+    handleButtonClick()
+}
+// Метод парсит указанную строку как HTML и добавляет результирующие узлы в указанное место DOM-дерева. Не делает повторный рендеринг для существующих элементов внутри элемента-родителя на котором используется
+function renderPhotoGalery(photoFromApi) {
+   refs.cardContainer.insertAdjacentHTML('beforeend', photoCardMarkup(photoFromApi))
+}
+
+function clearPageOnNewSearch() {
+    refs.cardContainer.innerHTML = '';
+}
+
+/// 
+
+const element = document.getElementById('.my-element-selector');
+
+function handleButtonClick() {
+    element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+    })
+};
---
+++ src/js/apiService.js
@@ -0,0 +1,43 @@
+const URL = "https://pixabay.com/api/"
+    const KEY = "22443315-0655a572bf532c2d4a9d9c050";
+    const perPage = "12";
+
+export default class ApiServicePixabey {
+    constructor() {
+        this.searchRequest = '';
+        this.page = 1;
+    }
+
+    async getApiCards() {
+    // console.log("Before request",this);
+    
+    try {
+        const response = await fetch(`${URL}?image_type=photo&orientation=horizontal&q=${this.searchRequest}&page=${this.page}&per_page=${perPage}&key=${KEY}`);
+        // const cardFromAPi = await response.json(); // response.json() на свое место возвращает Промис!!!
+        // this.incrementPage();
+        // // console.log("After request", this);
+        // // console.log(cardFromAPi.hits);
+        // return cardFromAPi.hits;
+        const { hits }= await response.json(); // response.json() на свое место возвращает Промис!!!
+        this.incrementPage();
+        return hits;
+    } catch (e) {
+        return console.log(e);
+        }
+    }
+    incrementPage() {
+    this.page += 1;
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+
+    get request() {
+        return this.searchRequest;
+    }
+
+    set request(newRequest) {
+        this.searchRequest = newRequest;
+    }
+}
